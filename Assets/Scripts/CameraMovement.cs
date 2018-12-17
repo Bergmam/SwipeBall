5b52b6e1d03cb041a216eb9d679368ef862f57e9
Anton Bergman <antonbergman_@hotmail.com>
2018-12-17 17:54:24 +0100

    init unity 2018.3 project

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public ThirdPersonCamera camera;
+    private Rigidbody ballRb;
+    private Vector3 ballDirection;
+
+    private void Awake()
+    {
+        SwipeDetector.OnSwipe += SwipeDetectorOnSwipe;
+        ballRb = transform.GetComponent<Rigidbody>();
+        ballDirection = transform.forward;
+        print("originalballdirection:" + ballDirection);
+    }
+
+    private void SwipeDetectorOnSwipe(SwipeData data)
+    {
+        switch (data.Direction.ToString()){
+            case "Up":
+                ballRb.AddForce(ballDirection * 400.0f);
+                break;
+            case "Down":
+                break;
+            case "Left":
+                ballDirection = Quaternion.Euler(0, -90, 0) * ballDirection;
+                ballRb.AddForce(ballDirection * 50.0f);
+                camera.ChangeAngle(-90);
+                break;
+            case "Right":
+                ballDirection = Quaternion.Euler(0, 90, 0) * ballDirection;
+                ballRb.AddForce(ballDirection * 50.0f);
+                camera.ChangeAngle(90);
+                break;
+        }
+    }
+
+    public Vector3 getBallDirection () {
+        return this.ballDirection;
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class ThirdPersonCamera : MonoBehaviour {
+  
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+    private float speed = 1.0f;
+
+    private float startTime;
+    private float journeyLength;
+  
+
+    void Start () {
+      offset = new Vector3(player.position.x, player.position.y + 8.0f, player.position.z - 7.0f);
+    }
+
+    void LateUpdate()
+    {
+        // offset = Quaternion.AngleAxis(directionAngle, Vector3.up) * offset;
+        // transform.position = player.position + offset;
+     // float distCovered = (Time.time - startTime) * speed;
+      //float fracJourney = distCovered / journeyLength;
+
+     // transform.position = Vector3.Lerp(transform.position, player.position + offset);
+      transform.position = player.position + offset;
+      transform.LookAt(player.position);
+    }
+
+    public void ChangeAngle(float angle) {
+      offset = Quaternion.AngleAxis(angle, Vector3.up) * offset;
+    }
+  }
